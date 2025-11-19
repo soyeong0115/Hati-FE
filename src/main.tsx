6e8fd81dec9ba576078e8670e3d8c6b61683371f
soyeong <mb535622@sookmyung.ac.kr>
2025-11-19 14:51:32 +0900

    chore: 불필요한 파일 정리

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
 import { HelmetProvider } from 'react-helmet-async';
-import { store } from './store/store';
 import App from './App.tsx';
 import './index.css';
 
@@ -15,11 +13,9 @@ createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <HelmetProvider>
       <QueryClientProvider client={queryClient}>
-        <Provider store={store}>
-          <BrowserRouter>
-            <App />
-          </BrowserRouter>
-        </Provider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
         <ReactQueryDevtools initialIsOpen={false} />
       </QueryClientProvider>
     </HelmetProvider>
