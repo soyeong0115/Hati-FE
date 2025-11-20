616a086dcaccbfcd8bf988d925993e173f878901
soyeong <mb535622@sookmyung.ac.kr>
2025-11-21 02:07:23 +0900

    fix: 링크 공유 시 미리보기 이미지 표시 개선

--- src/components/common/SEO.tsx
+++ src/components/common/SEO.tsx
@@ -25,6 +25,9 @@ export default function SEO({
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
+      <meta property="og:image:width" content="1200" />
+      <meta property="og:image:height" content="630" />
+      <meta property="og:image:type" content="image/png" />
 
       {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
